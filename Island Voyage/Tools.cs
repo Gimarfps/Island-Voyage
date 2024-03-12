0d653301fbde72a1c101b876f812eb45d68b4fa6
AkhirBey <AkhirBey@916-IAM-142-13>
2024-03-12 18:23:48 -0500

    Add project files.

---
+++ Island Voyage/Tools.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Island_Voyage
+{
+    internal class Tools
+    {
+        private List<Tool> Tools;
+        private Player player = new Player();
+        //Different Tools
+        //Shovel - Bronze amulet and the sparkling water
+        //Drill - 
+        //Head lamp - 
+        //
+        //
+
+        //
+        //
+
+        //
+    }
+}
